f87592c0993922494d762ac296fd21be86a8c7d7
meta-shobhit-bhatnagar <shobhit.bhatnagar@metacube.com>
2019-07-18 23:51:12 +0530

    Assignment 4 first commit

---
+++ SCF-Session4-Assignment/src/com/shobhit/ArrOperation.java
@@ -0,0 +1,148 @@
+package com.shobhit;
+
+import java.util.*;
+/**
+ * 
+ * @author Shobhit
+ *
+ */
+
+public class ArrOperation {
+	
+	public static int largestMirrorSize(int[] arr)throws AssertionError {
+		
+		 
+			int lengthOfMirror = 0;
+			int tempLength = 0;
+			if(arr.length==0){
+				throw new AssertionError("Null Array Found");
+			}else{
+				for(int i=0; i<arr.length; i++){
+					int pivotIndex=i;
+					tempLength=0;
+					for(int j=arr.length-1; j>=i;){
+						if(arr[pivotIndex]==arr[j]){
+							tempLength++;
+							pivotIndex++;
+							j--;
+						}else if(tempLength > 0){
+								if(tempLength > lengthOfMirror){
+								lengthOfMirror = tempLength;
+								tempLength = 0;
+								pivotIndex = i;
+							}else{
+								tempLength = 0;
+								pivotIndex = i;
+							}	
+						}else{
+							tempLength = 0;
+							pivotIndex = i;
+							j--;
+						}
+					}
+					if(tempLength>lengthOfMirror) lengthOfMirror=tempLength;
+					
+				}
+			}
+			
+			return lengthOfMirror;
+		}
+	
+	
+	
+	public static int numberOfClumps(int[] arrOfIntegers) throws AssertionError{
+		int countAdjacent = 0;
+		int countClumps = 0;
+		if(arrOfIntegers.length == 0){
+			throw new AssertionError("Empty Array Found");
+		}else{
+			for(int i=0; i<arrOfIntegers.length-1; i++){
+				if(arrOfIntegers[i] == arrOfIntegers[i+1]){
+					countAdjacent++;
+				}else if(countAdjacent > 0){
+					countClumps++;
+					countAdjacent = 0;
+				}else{
+					continue;
+				}
+			}
+			if(countAdjacent > 0){
+				countClumps++;
+			}
+		}
+		
+		return countClumps;
+	}
+	
+	
+	public static int[] fixXY(int X, int Y, int[] arrOfIntegers) throws AssertionError{
+		int newArrayAfterFixXY[] = new int[arrOfIntegers.length];
+		if(arrOfIntegers.length == 0){
+			throw new AssertionError("Empty Array Found");
+		}else{
+			ArrayList<Integer> indexListOfY = new ArrayList<Integer>();
+			int numberOfX = 0;
+			for(int i=0; i<arrOfIntegers.length; i++){
+				newArrayAfterFixXY[i] = arrOfIntegers[i];
+				if(X== arrOfIntegers[i]){
+					numberOfX++;
+				}
+				if(Y == arrOfIntegers[i]){
+					indexListOfY.add(i);
+				}else{
+					continue;
+				}
+			}
+			if(indexListOfY.size() == 0){
+				throw new AssertionError("Y is not Found");
+			}else if(numberOfX != indexListOfY.size()){
+				throw new AssertionError("X and Y is unequal");
+			}else{
+				int indexOfList = 0;
+				for(int i=0; i<newArrayAfterFixXY.length ; i++){
+					if(X == newArrayAfterFixXY[i]){
+						if( i == newArrayAfterFixXY.length -1){
+							throw new AssertionError("X occurs at last index");
+						}else{
+							if(newArrayAfterFixXY[i] == newArrayAfterFixXY[i+1]){
+								throw new AssertionError("Two Adjacent values is here");
+							}else{
+								int tempValue = newArrayAfterFixXY[i+1];
+								newArrayAfterFixXY[i+1] = newArrayAfterFixXY[indexListOfY.get(indexOfList)];
+								newArrayAfterFixXY[indexListOfY.get(indexOfList++)] = tempValue;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return newArrayAfterFixXY;
+	}
+	
+	
+	public static int indexOfSplitArray(int[] arrOfIntegers){
+		int index = -1;
+		if(arrOfIntegers.length == 0){
+			throw new AssertionError("Null Array Found");
+		}else{
+			int leftSideSum = 0;
+			int rightSideSum = 0;
+			for(int i =0; i<arrOfIntegers.length ; i++){
+				rightSideSum += arrOfIntegers[i]; 
+			}
+			for(int i=0; i<arrOfIntegers.length; i++){
+				if(leftSideSum<rightSideSum){
+					leftSideSum += arrOfIntegers[i];
+					rightSideSum -= arrOfIntegers[i];
+				}else if(leftSideSum == rightSideSum){
+					index = i;
+					break;
+				}else{
+					break;
+				}
+			}
+		}		
+		return index;
+	}
+}
---
+++ SCF-Session4-Assignment/src/com/shobhit/Main.java
@@ -0,0 +1,101 @@
+package com.shobhit;
+
+import java.util.*;
+
+/**
+ * 
+ * @author Shobhit
+ *
+ */
+
+public class Main {
+	public static void main(String[] args){
+		
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter length of array :");
+		int[] arr=null;
+		try{
+			int lengthOfArray= sc.nextInt();
+			arr= new int[lengthOfArray];
+			for(int i = 0; i<lengthOfArray;){
+				try{
+					System.out.println("Enter value of " + (i+1) + " element");
+					arr[i] = sc.nextInt();
+					if(arr[i] < 1){
+						System.out.println("Enter positive integer only");
+					}else{
+						i++;
+					}
+				}catch(InputMismatchException e){
+					System.out.println("That's not an Integer !! Try Again ");
+					sc.next();
+				}
+			}
+		}
+		catch(NegativeArraySizeException e){
+			System.out.println("Array length cannot be a negative integer! Enter again");
+			main(args);
+		}catch(InputMismatchException e){
+			System.out.println("That's not an integer! Enter again");
+			main(args);
+		}
+		
+		int choice=0;
+		
+		do{
+			try{
+				printInstructions();
+				System.out.println("Enter your choice :");
+				choice= sc.nextInt();
+				
+				switch(choice){
+				case 1: 
+					System.out.println("Size of the largest Mirror section : " + ArrOperation.largestMirrorSize(arr));
+					break;
+				case 2:
+					System.out.println("Number of clumps :"+ ArrOperation.numberOfClumps(arr));
+					break;
+				case 3:
+					int X,Y;
+					System.out.println("Enter value of X:");
+					X = sc.nextInt();
+					System.out.println("Enter value of Y:");
+					Y = sc.nextInt();
+					int[] rearrangedArray= ArrOperation.fixXY(X, Y, arr);
+					System.out.println("Rearranged array after fix :");
+					for(int i=0; i<arr.length; i++){
+						System.out.print(" " + arr[i]);
+					}
+					
+					break;
+				case 4:
+					System.out.println("Split Index of array  : " + ArrOperation.indexOfSplitArray(arr));
+				
+				case 5:
+					break;
+					default: 
+						System.out.println("Invalid choice! Enter between 0 and 5");
+				}
+				
+				
+			}catch(AssertionError e){
+				System.out.println("Assertion Error : " + e);
+			}catch(InputMismatchException e){
+				System.out.println("Enter integer only!");
+			}
+		}while(choice!=5);
+		
+	}
+	
+	
+	public static void printInstructions(){
+		System.out.println("1.Largest Mirror Section");
+		System.out.println("2.Number of clumps");
+		System.out.println("3.Array after fixXY");
+		System.out.println("4.Split Index of array");
+		System.out.println("5.Exit");
+	}
+	
+	
+}
