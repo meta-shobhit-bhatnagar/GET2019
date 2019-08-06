f12865161728825e36bbf59d7187c09665e79f07
unknown <shobhit.bhatnagar@metacube.com>
2019-08-06 17:04:50 +0530

    test case q1 override equals()

--- DS-Session5-Assignment/src/com/shobhit/q2/LinkedListEmployee.java
+++ DS-Session5-Assignment/src/com/shobhit/q2/LinkedListEmployee.java
@@ -15,6 +15,46 @@ public class LinkedListEmployee {
 		
 	}
 	
+	
+	/*@Override
+    public boolean equals(Object obj) 
+    { 
+          
+    // checking if both the object references are  
+    // referring to the same object. 
+    if(this == obj) 
+            return true; 
+          
+        // it checks if the argument is of the  
+        // type Geek by comparing the classes  
+        // of the passed argument and this object. 
+        // if(!(obj instanceof Geek)) return false; ---> avoid. 
+        if(obj == null || obj.getClass()!= this.getClass()) 
+            return false; 
+          
+        // type casting of the argument.  
+        LinkedListEmployee eList = (LinkedListEmployee) obj; 
+          
+        // comparing the state of argument with  
+        // the state of 'this' Object. 
+        return (eList.head.data.getName() == this.head.data.getName()); 
+    } 
+      
+    @Override
+    public int hashCode() 
+    { 
+          
+        // We are returning the Geek_id  
+        // as a hashcode value. 
+        // we can also return some  
+        // other calculated value or may 
+        // be memory address of the  
+        // Object on which it is invoked.  
+        // it depends on how you implement  
+        // hashCode() method. 
+        return 1; 
+    } */
+	
 	/**
 	 * The method adds new Node to the linked list with Employee data
 	 * @param new_data 
--- DS-Session5-Assignment/src/com/shobhit/q2/LinkedListEmployeeTestCase.java
+++ DS-Session5-Assignment/src/com/shobhit/q2/LinkedListEmployeeTestCase.java
@@ -7,41 +7,32 @@ import org.junit.Test;
 
 public class LinkedListEmployeeTestCase {
 
+	LinkedListEmployee list = new LinkedListEmployee();
 	@Before
 	public void Initialize(){
 
-	LinkedListEmployee list = new LinkedListEmployee();
+	
 
-	Employee e1 = new Employee(4000, "Shobhit", 21);
-	Employee e2 = new Employee(8000.0, "Prerna", 24);
-	Employee e3 = new Employee(6000.0, "Himanshu", 28);
-	Employee e4 = new Employee(10000.0, "Akhilesh", 34);
-	Employee e5 = new Employee(2000.0, "Govinda", 22);
-
-	list.append(e1);
-	list.append(e2);
-	list.append(e3);
-	list.append(e4);
-	list.append(e5);
+	list.append(new Employee(4000, "Shobhit", 21));   
+	list.append(new Employee(8000.0, "Prerna", 24));
+	list.append(new Employee(6000.0, "Himanshu", 28));
+	list.append(new Employee(10000.0, "Akhilesh", 34));
+	list.append(new Employee(2000.0, "Govinda", 22));
 
 	}	
 
 	@Test
 	public void EmployeeLinkedListSortingTest() {
 		LinkedListEmployee list2 = new LinkedListEmployee();
-	Employee e4 = new Employee(10000.0, "Akhilesh", 34);
-	Employee e5 = new Employee(2000.0, "Govinda", 22);
-	Employee e1 = new Employee(4000, "Shobhit", 21);
-	Employee e2 = new Employee(8000.0, "Prerna", 24);
-	Employee e3 = new Employee(6000.0, "Himanshu", 28);
-	list2.append(e5);
-	list2.append(e1);
-	list2.append(e2);
-	list2.append(e3);
-	list2.append(e4);
+	
+	list2.append(new Employee(10000.0, "Akhilesh", 34));
+	list2.append(new Employee(4000, "Shobhit", 21));
+	list2.append(new Employee(8000.0, "Prerna", 24));
+	list2.append(new Employee(6000.0, "Himanshu", 28));
+	list2.append(new Employee(10000.0, "Akhilesh", 34));
 
 
-	//assertEquals(list2.getHead().data.toString(),list.mergeSort(list.head).data.toString());
+	assertEquals(list2 , list.mergeSort(list.head));
 	}
 
 }
