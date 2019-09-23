d4b4995f86be3e61063d72ee9c48603750425354
unknown <shobhit.bhatnagar@metacube.com>
2019-09-23 18:42:07 +0530

    EAD-Session10

---
+++ EAD-Session10/src/main/java/com/metacube/EADSession9/EadSession9Application.java
@@ -0,0 +1,18 @@
+package com.metacube.EADSession9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * main class for spring application
+ * @author Shobhit Bhatnagar
+ *
+ */
+@SpringBootApplication
+public class EadSession9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession9Application.class, args);
+	}
+
+}
